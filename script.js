ebeab011f53326afdf50432350abae216123d571
Ahmed Zahid <157976515+Ahmedzahid7@users.noreply.github.com>
2025-06-20 15:30:40 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add click event to the button
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("clickButton");
+    button.addEventListener("click", () => {
+        alert("Thanks for clicking the button!");
+    });
+});
